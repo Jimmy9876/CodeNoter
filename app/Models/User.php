091f44bd25d31fe3cb9edb394216d3dc36b40c5a
Jimmy <jiwu@jideMacBook-Pro.local>
2017-12-07 19:54:42 +0800

    Notify author when topic replied

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,15 +5,14 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+use Auth;
+
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable {
+        notify as protected laravelNotify;
+    }
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password', 'introduction', 'avatar',
     ];
@@ -27,11 +26,21 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    public function notify($instance)
+    {
+        // 如果要通知的人是当前用户，就不必通知了！
+        if ($this->id == Auth::id()) {
+            return;
+        }
+        $this->increment('notification_count');
+        $this->laravelNotify($instance);
+    }
+
     public function replies()
     {
         return $this->hasMany(Reply::class);
     }
-    
+
     public function topics()
     {
         return $this->hasMany(Topic::class);
@@ -40,5 +49,12 @@ class User extends Authenticatable
     public function isAuthorOf($model)
     {
         return $this->id == $model->user_id;
+    }
+
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
     }
 }
